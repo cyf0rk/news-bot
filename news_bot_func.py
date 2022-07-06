620146c9c7e68542a4dc4e32eceb8a80e7bbb822
marijan111 <marijanbatarilo.dev@gmail.com>
2022-07-06 18:09:08 +0200

    use finviz as news api

--- news_bot_func.py
+++ news_bot_func.py
@@ -1,12 +1,13 @@
 import discord
-import fetch_news
+import finviz_news
 import api_client
 
 from datetime import datetime
 
 api_client = api_client.ApiClient()
-news_api = fetch_news.NewsApi()
+news_api = finviz_news.FinvizNews()
 client = discord.Client()
+news_amount = 3;
 
 @client.event
 async def on_ready():
@@ -19,14 +20,14 @@ async def on_message(message):
 
     if message.content.startswith('$') and len(message.content) <= 6 and message.content.isupper():
         message_content = message.content.replace('$', '')
-        news = news_api.getNews(message_content)
+        news_api.get_news(message_content)
+        news = news_api.news
 
-        for item in news[0:3]:
-            first_news_title = item['title']
-            first_news_summary = item['summary']
-            first_news_url = item['url']
-            date = datetime.strptime(item['time_published'], '%Y%m%dT%H%M%S')
+        for item in news[0:news_amount]:
+            date = item[0]
+            first_news_title = item[1]
+            first_news_url = item[2]
 
-            await message.channel.send(f'{date}\n{first_news_title}\n\n{first_news_summary}\n{first_news_url}')
+            await message.channel.send(f'{date}\n{first_news_title}\n{first_news_url}')
 
 client.run(api_client.discord_key)
